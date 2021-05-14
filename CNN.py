8a35320ee8efe63dc42a161beb04643e10f4cecc
thinhemb <thinhdo19022001@gmail.com>
2021-05-14 16:56:12 +0700

    fix tf.keras.utils

--- CNN.py
+++ CNN.py
@@ -1,3 +1,4 @@
+#%%
 import keras
 import numpy as np
 import tensorflow 
@@ -5,7 +6,7 @@ from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Input
 from keras.models import Model, Sequential
 from keras.datasets import mnist
 from tensorflow.keras.utils import to_categorical
-from keras.utils import plot_model
+from tensorflow.keras.utils import plot_model
 import matplotlib.pyplot as plt
 #load dataset
 (X_train, y_train), (X_test, y_test)= mnist.load_data()
@@ -54,27 +55,34 @@ model.compile(optimizer='Adam',
             )
 #Thực hiện train model với data
 model=model.fit(X_train, y_train, batch_size=8, epochs= 10, validation_data=(X_test, y_test))
-
+#%%
 #Vẽ đồ thị loss, accuracy của training set và validation set
 fig = plt.figure()
 numOfEpoch = 10
 plt.plot(np.arange(0, numOfEpoch), model.history['loss'], label='training loss')
 plt.plot(np.arange(0, numOfEpoch), model.history['val_loss'], label='validation loss')
-plt.plot(np.arange(0, numOfEpoch),model.history['acc'], label='accuracy')
-plt.plot(np.arange(0, numOfEpoch), model.history['val_acc'], label='validation accuracy')
+plt.plot(np.arange(0, numOfEpoch),model.history['accuracy'], label='accuracy')
+plt.plot(np.arange(0, numOfEpoch), model.history['val_accuracy'], label='validation accuracy')
 plt.title('Accuracy and Loss')
 plt.xlabel('Epoch')
 plt.ylabel('Loss|Accuracy')
 plt.legend()
+#%%
+
+model.save("fuck.h5")
+
+#%%
+model=model.history['accuracy']
 #Đánh giá model với dữ liệu test set
+# Evaluate the model on the test data using `evaluate`
+print("Evaluate on test data")
 score = model.evaluate(X_test, y_test, verbose=0)
 print(score)
-
+#%%
 #. Dự đoán ảnh
 plt.imshow(X_test[0].reshape(28,28), cmap='gray')
 y_predict = model.predict(X_test[0].reshape(1,28,28,1))
 print('Giá trị dự đoán: ', np.argmax(y_predict))
 
 
-model.save("fuck.h5")
-
+# %%
